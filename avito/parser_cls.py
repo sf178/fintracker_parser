25257af60a0639c7e3234b171669f0ca2d816d62
sf178 <dmitrynekrylov@yandex.ru>
2024-01-30 05:33:08 +0300

    first

--- avito/parser_cls.py
+++ avito/parser_cls.py
@@ -18,7 +18,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from django.urls import reverse
-from test_backend import settings
+from fintracker_parser import settings
 
 
 from notifiers.logging import NotificationHandler
