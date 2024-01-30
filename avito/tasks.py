7f69eaea5fbb25faa1a21777388ced2e785b1de6
sf178 <dmitrynekrylov@yandex.ru>
2024-01-30 07:35:51 +0300

    first

--- avito/tasks.py
+++ avito/tasks.py
@@ -1,3 +1,6 @@
+import json
+
+import requests
 from celery import shared_task
 from .parser_cls import AvitoParse  # Импортируйте ваш парсер
 import configparser
@@ -5,12 +8,12 @@ import re
 from .city import *
 
 config = configparser.ConfigParser()  # создаём объекта парсера
-config.read("/Users/dmitrynekrylov/inmanage-back/glob_parse/settings.ini")  # читаем конфиг
+config.read("/app/avito/settings.ini")  # читаем конфиг
 try:
     """Багфикс проблем с экранированием"""
     url = config["Avito"]["URL"]  # начальный url
 except Exception:
-    with open('/Users/dmitrynekrylov/inmanage-back/glob_parse/settings.ini') as file:
+    with open('/app/avito/settings.ini') as file:
         line_url = file.readlines()[1]
         regex = r"http.+"
         url = re.search(regex, line_url)[0]
@@ -28,3 +31,52 @@ def parse_avito_task(src, property_id, city, square):
                         count=int(num_ads), min_price=min_price, max_price=max_price, keysword_list=keys)
     parser.parse()
 
+
+def get_exchange_rates(api_key, base='USD'):
+    url = f"https://openexchangerates.org/api/latest.json?app_id={api_key}&base={base}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return json.loads(response.text)
+    else:
+        print("Ошибка при получении данных:", response.status_code)
+        return None
+
+
+def convert_to_rub(rates, rub_rate):
+    converted_rates = {}
+    for currency, rate in rates.items():
+        # Пересчет курса валюты к RUB
+        converted_rates[currency] = rub_rate / rate
+    return converted_rates
+
+
+def get_rates_in_rub(api_key, currencies):
+    rates = get_exchange_rates(api_key)
+    if rates:
+        rub_exchange_rate = rates['rates']['RUB']
+        rates_in_rub = convert_to_rub(rates['rates'], rub_exchange_rate)
+        return {currency: rates_in_rub[currency] for currency in currencies}
+
+
+@shared_task
+def currency_rates_task():
+    api_key = 'db8d9f75688041cf831131e1b35655e3'  # Установите ваш API ключ
+    currencies = ['EUR', 'GBP', 'JPY', 'CNY', 'USD']  # Выбранные валюты
+
+    rates_in_rub = get_rates_in_rub(api_key, currencies)
+    if rates_in_rub:
+        # URL внешнего сервера, на который вы отправляете запрос
+        url = 'http://194.87.252.100/balance/currency/'
+
+        # Отправка POST-запроса на внешний сервер
+        try:
+            response = requests.post(url, json=rates_in_rub)
+            response.raise_for_status()
+            # Обработка успешного ответа
+            return response.json()
+        except requests.RequestException as e:
+            # Обработка ошибок сети и HTTP-ответов, указывающих на ошибку
+            print(f"Ошибка при отправке запроса: {e}")
+            return {'error': str(e)}
+    else:
+        return {'error': 'Не удалось получить данные о курсах валют'}
